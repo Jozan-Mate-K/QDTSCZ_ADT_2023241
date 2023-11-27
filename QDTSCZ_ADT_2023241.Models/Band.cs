e5d6c861ed18028e2a5dced377a0ea8c4b5350ab
Józan Máté <jozan.mate17@gmail.com>
2023-11-27 16:31:49 +0100

    Db change

--- QDTSCZ_ADT_2023241.Models/Band.cs
+++ QDTSCZ_ADT_2023241.Models/Band.cs
@@ -4,17 +4,21 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace QDTSCZ_ADT_2023241.Models
 {
-    internal class Band
+    [Table("Bands")]
+    public class Band
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public int Balance { get; set; }
+        [NotMapped]
+        [JsonIgnore]
         public virtual ICollection<Instrument> RequiredInstruments { get; set; }
         [Range(1, 5)]
         public int Priority {  get; set; }
--- QDTSCZ_ADT_2023241.Models/Instrument.cs
+++ QDTSCZ_ADT_2023241.Models/Instrument.cs
@@ -1,18 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace QDTSCZ_ADT_2023241.Models
 {
     public enum instrumentTypeEnum {STRINGS, PERCUSSION, WIND, SYNTH }
-    internal class Instrument
+    [Table("Instruments")]
+    public class Instrument
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public int Year { get; set; }
+        [Required]
         public instrumentTypeEnum Type{ get; set; }
         public string Color {  get; set; }
+        [NotMapped]
+        [JsonIgnore]
         public virtual Manufacturer Manufacturer { get; set; }
+        [ForeignKey(nameof(Manufacturer))]
+        public int ManufacturerId { get; set; }
+        [NotMapped]
+        [JsonIgnore]
+        public virtual Band Band { get; set; }
+        [ForeignKey(nameof(Band))]
+        public int BandId { get; set; }
     }
 }
