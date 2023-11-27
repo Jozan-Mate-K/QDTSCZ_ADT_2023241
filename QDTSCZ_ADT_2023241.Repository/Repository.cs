05912596f6bc9c48ad74c79cea05bcf2c00e6a4f
Józan Máté <jozan.mate17@gmail.com>
2023-11-27 18:51:21 +0100

    Abstract repo added

---
+++ QDTSCZ_ADT_2023241.Repository/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QDTSCZ_ADT_2023241.Repository
+{
+    public abstract class Repository<T> where T : class
+    {
+        protected Context context;
+        protected Repository(Context context)
+        {
+            this.context = context;
+        }
+        public void Add(T element)
+        {
+            context.Set<T>().Add(element);
+            context.SaveChanges();
+        }
+        public void Remove(T element)
+        {
+            context.Set<T>().Remove(element);
+            context.SaveChanges();
+        }
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+        public abstract T GetSingle(int ID);
+    }
+}
