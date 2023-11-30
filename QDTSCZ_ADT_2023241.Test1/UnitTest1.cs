cd9da4f64e67ee7c08545b49e7d1dbb5b0e4967b
Józan Máté <jozan.mate17@gmail.com>
2023-11-30 11:40:32 +0100

    CF commit

---
+++ QDTSCZ_ADT_2023241.Test1/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace QDTSCZ_ADT_2023241.Test1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
